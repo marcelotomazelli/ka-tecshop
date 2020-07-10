ce92b1e281b950e8b6367f54fb77e0ff1bb511c8
Marce-T <marcelo.tomazelli@hotmail.com>
2020-07-09 21:37:28 -0300

    carousel
    carousel agora esta completamente adaptavel a outros tipos de itens, tambem foi alterada as funções de onload e onresize, agora chama um method static de uma class feita para redimencionar o carrouseis

--- js/script.js
+++ js/script.js
@@ -1,63 +1,23 @@
-let carousel_intro_width, carousel_intro_height;
-let carousel_interval;
-let carousel_obj_intro;
-
-function onloadBody() {
-	let carousel_intro = document.getElementById('carousel-introduction')
-	let carousel_width = carousel_intro.offsetWidth
-	carousel_intro.style.height = (carousel_width / 2) + 'px'
-
-	carousel_intro_width = carousel_width
-	carousel_intro_height = carousel_width / 2
-
-	let carousel_imgs = document.getElementsByClassName('carousel-image')
-
-	for(let i = 0; i < carousel_imgs.length; i++) {
-		carousel_imgs[i].style.width = carousel_intro_width + 'px'
-		carousel_imgs[i].style.height = carousel_intro_height + 'px'
-	}
-
-	// Carousel da introdução
-	carousel_obj_intro = new Carousel(items_carousel_intro, carousel_intro_width, 4000)
-}
-
-function resizingWindow() {
-	let carousel_intro = document.getElementById('carousel-introduction')
-	let carousel_width = carousel_intro.offsetWidth
-	carousel_intro.style.height = (carousel_width / 2) + 'px'
-
-	carousel_intro_width = carousel_width
-	carousel_intro_height = carousel_width / 2
-
-	let carousel_imgs = document.getElementsByClassName('carousel-image')
-
-	for(let i = 0; i < carousel_imgs.length; i++) {
-		carousel_imgs[i].style.width = carousel_intro_width + 'px'
-		carousel_imgs[i].style.height = carousel_intro_height + 'px'
-	}
-
-	carousel_obj_intro.resizing(carousel_intro_width)
-}
-
 class Carousel {
 
-	constructor(reference_items, carousel_slide_width, time_interval) {
-		this.reference_items = reference_items
+	constructor(reference_items, carousel_slide_width, time_interval, last_visible_elements) {
+		this.reference_items = document.getElementsByClassName(reference_items)
 		this.carousel_slide_count = 0
 		this.carousel_slide_width = carousel_slide_width
 		this.time_interval = time_interval
 		this.interval = this.intervalControl(this.time_interval)
-		console.log(this.interval)
+		this.last_visible_elements = last_visible_elements
+	}
+
+	updateValues(new_carousel_slide_width, new_last_visible_elements) {
+		this.carousel_slide_width = new_carousel_slide_width
+		this.last_visible_elements = new_last_visible_elements
 	}
 
 	intervalControl(time) {
 		return setInterval(() => {
 			this.autonext()
 		}, time)
-	}
-
-	updateValues(new_carousel_slide_width) {
-		this.carousel_slide_width = new_carousel_slide_width
 	}
 
 	moveItems() {
@@ -70,7 +30,7 @@ class Carousel {
 		clearInterval(this.interval)
 		this.carousel_slide_count += this.carousel_slide_width
 
-		if(this.carousel_slide_count == this.carousel_slide_width * this.reference_items.length) {
+		if(this.carousel_slide_count == this.carousel_slide_width * (this.reference_items.length - this.last_visible_elements + 1)) {
 			this.carousel_slide_count = 0
 		}
 		this.moveItems()
@@ -80,7 +40,7 @@ class Carousel {
 	prev() {
 		clearInterval(this.interval)
 		if(this.carousel_slide_count == 0) {
-			this.carousel_slide_count = this.carousel_slide_width * (this.reference_items.length -1)
+			this.carousel_slide_count = this.carousel_slide_width * (this.reference_items.length - this.last_visible_elements)
 		} else {
 			this.carousel_slide_count -= this.carousel_slide_width
 		}
@@ -90,20 +50,94 @@ class Carousel {
 
 	autonext() {
 		this.carousel_slide_count += this.carousel_slide_width
+		let aux = this.carousel_slide_width * (this.reference_items.length - this.last_visible_elements + 1)
 		
-		if(this.carousel_slide_count == this.carousel_slide_width * this.reference_items.length) {
+		if(this.carousel_slide_count >= aux) {
 			this.carousel_slide_count = 0
 		}
 		this.moveItems()
+
 	}
 
-	resizing(new_carousel_slide_width) {
+	resizing(new_carousel_slide_width, new_last_visible_elements) {
 		clearInterval(this.interval)
 		this.carousel_slide_count = 0
 		this.moveItems()
-		this.updateValues(new_carousel_slide_width)
+		this.updateValues(new_carousel_slide_width, new_last_visible_elements)
 		this.interval = this.intervalControl(this.time_interval)
 	}
 }
 
-let items_carousel_intro = document.getElementsByClassName('carousel-item')
+class reziseCarousel {
+	constructor() {
+	}
+
+	static carouselImages(items, id, size = null) {
+		let item = document.getElementById(id)
+		let w = item.offsetWidth
+		let h = w / size
+		item.style.height = h + 'px'
+
+		let list = document.getElementsByClassName(items)
+
+		for(let i = 0; i < list.length; i++) {
+			list[i].style.width = w + 'px'
+			list[i].style.height = h + 'px'
+		}
+		return w
+	}
+
+	static carouselSlideItem(items, min, max) {
+		let list = document.getElementsByClassName(items)
+		let w = document.getElementsByClassName('content')[0].offsetWidth
+
+		if(window.innerWidth < min) {
+			w /= 2
+			w = Math.round(w)
+			for(let i = 0; i < list.length; i++) {
+				list[i].style.minWidth = w + 'px'
+			}
+		} else if(window.innerWidth >= min && window.innerWidth < max) {
+			w /= 3
+			w = Math.round(w)
+			for(let i = 0; i < list.length; i++) {
+				list[i].style.minWidth = w + 'px'
+			}
+		} else {
+			for(let i = 0; i < list.length; i++) {
+				list[i].style = ''
+			}
+			w = document.getElementsByClassName(items)[0].offsetWidth
+			w = Math.round(w)
+		}
+
+		return w
+	}
+}
+
+let carousel_obj_intro;
+let carousel_obj_trend;
+
+function onloadBody() {
+	carousel_intro_width = reziseCarousel.carouselImages('carousel-image', 'carousel-introduction', 2)
+
+	let content_width = document.getElementsByClassName('content')[0].offsetWidth
+	let slide_width_1 = reziseCarousel.carouselSlideItem('slide_items_1', 620, 768)
+	let last_slide_elements = Math.round(content_width / slide_width_1)
+
+	// Carousel da introdução
+	carousel_obj_intro = new Carousel('carousel-item', carousel_intro_width, 4000, 1)
+	carousel_obj_trend = new Carousel('slide_items_1', slide_width_1, 7000, last_slide_elements)
+}
+
+function resizingWindow() {
+	carousel_intro_width = reziseCarousel.carouselImages('carousel-image', 'carousel-introduction', 2)
+
+	let slide_content = document.getElementsByClassName('content')[0].offsetWidth
+	let slide_items_width_1 = reziseCarousel.carouselSlideItem('slide_items_1', 620, 768)
+
+	let last_view_elements = Math.round(slide_content / slide_items_width_1)
+
+	carousel_obj_intro.resizing(carousel_intro_width, 1)
+	carousel_obj_trend.resizing(slide_items_width_1, last_view_elements)
+}
